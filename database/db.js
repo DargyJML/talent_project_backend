94e02fc64e74c6d61585c80dee29e5f6ed55ea29
Dargy <dargijuli@gmail.com>
2022-10-23 23:34:44 -0500

    arreglo del error del puerto para conexion con bd

--- database/db.js
+++ database/db.js
@@ -2,7 +2,9 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize('railway', 'root','4Vdn2fSIE3wqEmvSTXWG',{
     localhost:'containers-us-west-98.railway.app',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: '7310'
+
 })
 
 export default db
